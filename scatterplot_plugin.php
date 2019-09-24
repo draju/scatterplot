5fe02da707ebb19f802cb8e6ee793a1214bec9cf
vishnu <4461424+draju@users.noreply.github.com>
2019-09-24 13:49:00 -0400

    add call to REDCap::getData

--- scatterplot_plugin.php
+++ scatterplot_plugin.php
@@ -16,23 +16,57 @@ echo "<script src='https://cdn.plot.ly/plotly-latest.min.js'></script>";
 //echo "<PRE>".print_r($module->settings)."</PRE>";
 for($i=0; $i < count($module->settings); $i++){
     echo "<h3 class='scatterplot-em'>Scatter Plot #".($i+1).": ".$module->settings[$i]["plot-title"]."</h3><div id='myDiv$i' class='scatterplot-em'></div>";    
+    
+    $pid = $module->getProjectId();
+    $data = REDCap::getData($pid,'array',NULL,array($module->settings[$i]["x-coord"],$module->settings[$i]["y-coord"]));
+    $x_coord = array();
+    $y_coord = array();
+    $records = array();
+    foreach ($data as $rec => $ev_arr){
+        foreach ($ev_arr as $ev => $val){
+            array_push($x_coord,$val[$module->settings[$i]["x-coord"]]);
+            array_push($y_coord,$val[$module->settings[$i]["y-coord"]]);
+            array_push($records,$rec);
+        }
+    }
+
 ?>      
 <script>
       var trace1 = {
-        x: [1, 2, 3, 4, 5],
-        y: [1, 6, 3, 6, 1],
+        x: [
+        <?php 
+            for($j=0; $j<count($x_coord); $j++ ){
+                if($j > 0){
+                    echo ",";
+                }
+                echo $x_coord[$j];
+            } 
+        ?>
+        ],
+        y: [
+        <?php 
+            for($j=0; $j<count($y_coord); $j++ ){
+                if($j > 0){
+                    echo ",";
+                }
+                echo $y_coord[$j];
+            } 
+        ?>
+        ], 
         mode: 'markers',
         type: 'scatter'
       };
       
+      /*
       var trace2 = {
         x: [1.5, 2.5, 3.5, 4.5, 5.5],
         y: [4, 1, 7, 1, 4],
         mode: 'markers',
         type: 'scatter'
       };
-      
-      var data = [ trace1, trace2 ];
+      */
+
+      var data = [ trace1 ];
       
       var layout = {
         xaxis: {
